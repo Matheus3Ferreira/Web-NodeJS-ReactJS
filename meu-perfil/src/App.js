de6e5e0404010045791ba6ed5215e47d81def3a2
Matheus2798 <matheus.s.f@outlook.com.br>
2021-11-02 20:06:25 -0300

    add meu-perfil

--- meu-perfil/src/App.js
+++ meu-perfil/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+        <img src="https://avatars.githubusercontent.com/u/69362122?v=4" className="photo-profile" alt="photo" />
+        <h1 className="name">Matheus da Silva Ferreira</h1>
+
+        <main className="main-information">
+          
+          <section className="container-information">
+            <h3 className="title">Dados pessoais</h3>
+            <ul className="list">
+              <ol>Endereço: Rua São Domingos 490 Fundos, Morro São Bento, Santos-SP</ol>
+              <ol>Telefone: (13) 3224-4214</ol>
+              <ol>Celular: (13) 92000-4406</ol>
+            </ul>
+          </section>
+
+          <section className="container-information">
+            <h3 className="title">Formação</h3>
+            <ul className="list">
+              <ol>Ensino Médio: E.E. João Octavio dos Santos - completo em 2015</ol>
+              <ol>Ensino Superior:  Universidade Paulista - incompleto (2020-2021)</ol>
+            </ul>
+          </section>
+         
+          <section className="container-information">
+            <h3 className="title">Experiência</h3>
+            <ul className="list">
+              <ol>Auxiliar Administrativo: 1° Cartório de registro civil de Santos (2014-2017)</ol>
+              <ol>Monitor de Eventos: MEDGRUPO (2017-2019)</ol>
+            </ul>
+          </section>
+         
+          <section className="container-information">
+            <h3 className="title">Projetos</h3>
+            <ul className="list">
+              <ol>Web: <a href="https://github.com/Matheus2798/Web-NodeJS-ReactJS">NodeJS + ReactJS</a></ol>
+              <ol>Mobile: <a href="https://github.com/Matheus2798/React-Native-Expo">React Native</a></ol>
+            </ul>
+          </section>
+        </main>
+
     </div>
   );
 }
